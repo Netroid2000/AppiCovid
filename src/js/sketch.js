826278f7c9000be954a4566d068deaaae7dd47f9
Netroid2000 <triple-h-vs-edch@live.com.mx>
2021-05-30 01:07:35 -0600

    data

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,7 +1,19 @@
+let data;
+
+function preload() {
+  const url = "https://api.apify.com/v2/key-value-stores/vpfkeiYLXPIDIea2T/records/LATEST";
+  data = loadJSON(url);
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  console.log(data);
+
+  text(data.State["Aguascalientes"].infected, 50, 50);
+  noLoop();
 }
 
